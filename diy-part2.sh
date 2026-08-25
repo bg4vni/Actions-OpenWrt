e90986e02d9af63b35b9ada87cd95c7cc6b98bc3
bg4vni <72649376+bg4vni@users.noreply.github.com>
2026-08-25 10:40:25 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 #
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part2.sh
-# Description: OpenWrt DIY script part 2 (After Update feeds)
-#
-# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
 
-# Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
-# Modify default theme
-#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+# 修改普通版本dts，适配 112M 大分区 (ubi 分区大小改为 0x7000000)，扩容ubi到0x7000000
+sed -i 's/0x4000000/0x7000000/g' target/linux/mediatek/dts/mt7981b-h3c-magic-nx30-pro.dts
 
-# Modify hostname
-#sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+# 在h3c_magic‑nx30‑pro设备段下面追加UBIFS参数
+sed -i '/Device\/h3c_magic-nx30-pro/a UBIFS_OPTS:="-m 2048 -e 124KiB -c 11800"' target/linux/mediatek/image/filogic.mk
+
+# 修改默认LAN IP为192.168.5.1
+sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
+
+# 设置argon为默认主题
+cat > package/base-files/files/etc/uci-defaults/99-set-theme <<EOF
+#!/bin/sh
+uci set luci.main.mediaurlbase='/luci-static/argon'
+uci commit luci
+EOF
+chmod +x package/base-files/files/etc/uci-defaults/99-set-theme
