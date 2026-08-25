8305c0374346f026d63c05a76525fbca190c1836
bg4vni <72649376+bg4vni@users.noreply.github.com>
2026-08-25 11:29:34 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -8,6 +8,7 @@ sed -i 's/0x4000000/0x7000000/g' target/linux/mediatek/dts/mt7981b-h3c-magic-nx3
 # 替换filogic.mk下的IMAGE_SIZE
 sed -i '/define Device\/h3c_magic-nx30-pro/,/endef/s/IMAGE_SIZE := .*/IMAGE_SIZE := 114688k/' target/linux/mediatek/image/filogic.mk
 
+
 # 修改默认LAN IP为192.168.5.1
 sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
 
