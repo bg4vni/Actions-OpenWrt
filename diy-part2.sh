877199ee2b44fa4bdf5a6a25320d397c671a1fa4
bg4vni <72649376+bg4vni@users.noreply.github.com>
2026-08-25 10:50:44 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -5,8 +5,8 @@
 # 修改普通版本dts，适配 112M 大分区 (ubi 分区大小改为 0x7000000)，扩容ubi到0x7000000
 sed -i 's/0x4000000/0x7000000/g' target/linux/mediatek/dts/mt7981b-h3c-magic-nx30-pro.dts
 
-# 在h3c_magic‑nx30‑pro设备段下面追加UBIFS参数
-sed -i '/Device\/h3c_magic-nx30-pro/a UBIFS_OPTS:="-m 2048 -e 124KiB -c 11800"' target/linux/mediatek/image/filogic.mk
+# 替换filogic.mk下的IMAGE_SIZE
+sed -i '/define Device\/h3c_magic-nx30-pro/,/endef/s/IMAGE_SIZE := .*/IMAGE_SIZE := 114688k/' target/linux/mediatek/image/filogic.mk
 
 # 修改默认LAN IP为192.168.5.1
 sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
