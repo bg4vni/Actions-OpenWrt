64df03a540ec3003077bfa1e855870e3e2ecc7f9
bg4vni <72649376+bg4vni@users.noreply.github.com>
2026-08-27 09:56:58 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -2,3 +2,7 @@
 #
 # 1. 添加 Nikki 官方软件源
 echo 'src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git;main' >> feeds.conf.default
+
+# 拉取 PassWall 2 主插件与依赖包 (Openwrt-Passwall 官方新仓库)
+git clone https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git package/passwall_dep
+git clone https://github.com/Openwrt-Passwall/openwrt-passwall2.git package/passwall2
