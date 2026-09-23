bf6a3699d0073ce2b9d1a41924b6270f11195519
bg4vni <72649376+bg4vni@users.noreply.github.com>
2026-09-23 15:32:00 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -3,9 +3,6 @@
 # 1. 添加 Nikki 官方软件源
 echo 'src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git;main' >> feeds.conf.default
 
-
-
-
 # 2. 添加 PassWall 2 官方源（通过 feeds 机制拉取，会自动匹配系统依赖）
 echo 'src-git passwall_dep https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git;main' >> feeds.conf.default
 echo 'src-git passwall2 https://github.com/Openwrt-Passwall/openwrt-passwall2.git;main' >> feeds.conf.default
